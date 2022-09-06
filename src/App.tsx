de2a4beca7776fbfc294af44b1789704829e6aaf
谭苗苗 <939215882@qq.com>
2022-09-06 22:02:04 +0800

    修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,51 @@
 import {
-  LaptopOutlined,
-  NotificationOutlined,
+  EditOutlined,
+  ReadOutlined,
+  FormOutlined,
+  SelectOutlined,
   UserOutlined
 } from '@ant-design/icons'
 import { Breadcrumb, Layout, Menu } from 'antd'
-import type { MenuProps } from 'antd'
 import React from 'react'
 import './App.less'
 import { Outlet } from 'react-router-dom'
 import MyHeader from 'components/MyHeader'
 
-
 const { Content, Sider } = Layout
-const items2: MenuProps['items'] = [
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined
-].map((icon, index) => {
-  const key = String(index + 1)
-
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1
-      return {
-        key: subKey,
-        label: `option${subKey}`
-      }
-    })
+// 侧边栏数据
+const items2 = [
+  {
+    key: '1',
+    icon: <EditOutlined />,
+    label: '查看文章列表'
+  },
+  {
+    key: '2',
+    icon: <ReadOutlined />,
+    label: '文章编辑'
+  },
+  {
+    key: '3',
+    icon: <FormOutlined />,
+    label: '修改资料'
+  },
+  {
+    key: '4',
+    icon: <UserOutlined />,
+    label: '管理员',
+    children: [{ key: 'sub1', icon: <SelectOutlined />, label: '小编名单' }]
   }
-})
+]
+// 内容主体模拟数据
 
 const App = () => {
   return (
     <>
-      <Layout className='container'>
+      <Layout className="container">
         {/* 头部 */}
         <MyHeader></MyHeader>
-        <Layout className='container_content'>
+        <Layout className="container_content">
+          {/* {侧边栏} */}
           <Sider width={200} className="site-layout-background">
             <Menu
               mode="inline"
@@ -52,20 +57,15 @@ const App = () => {
             />
           </Sider>
           <Layout style={{ padding: '0 24px 24px' }}>
+            {/* 面包屑 */}
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>Home</Breadcrumb.Item>
               <Breadcrumb.Item>List</Breadcrumb.Item>
               <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
-            <Content
-              className="site-layout-background"
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280
-              }}
-            >
-              Content
+            {/* 內容主体 */}
+            <Content className="mycontent">
+              <Outlet></Outlet>
             </Content>
           </Layout>
         </Layout>
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,62 @@
+import { Button, Table } from 'antd'
+import { ColumnsType } from 'antd/lib/table/interface'
 import React from 'react'
-const List=()=>{
+
+interface DataType {
+  key: React.Key
+  title: React.ReactNode
+  time: string
+  action: React.ReactNode
+}
+const columns: ColumnsType<DataType> = [
+  {
+    title: '文章标题',
+    dataIndex: 'title',
+    width: '60%'
+  },
+  {
+    title: '发布时间',
+    dataIndex: 'time',
+    width: '25%'
+  },
+  {
+    title: '操作',
+    dataIndex: 'action',
+    width: '15%'
+  }
+]
+// 按钮组件
+const ActionBtn = () => {
   return (
-    <div>列表页</div>
+    <>
+      <Button type="primary" style={{ marginRight: '5px' }}>
+        编辑
+      </Button>
+      <Button type="primary" danger>
+        删除
+      </Button>
+    </>
   )
+}
+// 标题组件
+const TitleCop = () => (
+  <>
+    <div>
+      <a href="!#">标题123456789</a>
+    </div>
+    <p>副标题</p>
+  </>
+)
+const data: DataType[] = []
+for (let i = 0; i < 46; i++) {
+  data.push({
+    key: i,
+    title: <TitleCop />,
+    time: '123',
+    action: <ActionBtn />
+  })
+}
+const List = () => {
+  return <Table columns={columns} dataSource={data} showHeader={false} />
 }
 export default List
